c28a60a146ab20590a7765fb972a715cdabb4c51
NativeRV <nativerv@protonmail.com>
2021-05-19 19:07:33 +0300

    feat(basic-combat-system): projectile detects collision correctly

--- Assets/Scripts/Projectile/Projectile.cs
+++ Assets/Scripts/Projectile/Projectile.cs
@@ -13,6 +13,21 @@ public class Projectile : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+  }
+
+  private int stubValue = 0;
+
+  private void OnTriggerEnter(Collider other)
+  {
+    var othersHealth = other.gameObject.GetComponent<Health>();
+    if (!(othersHealth == null))
+    {
+      othersHealth.TakeDamage(stubValue++ % 10);
     }
+    else
+    {
+      Debug.Log($"Can't find health! Collided with: {other.gameObject.name}");
+    }
+  }
 }
